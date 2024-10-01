8f60c77ee304f3a0df717dee753f2363f8a160f9
a_daud <adam.daudrich@pm.me>
2024-10-01 11:27:13 -0400

    Update pos_neg_sentiment.py

--- pos_neg_sentiment.py
+++ pos_neg_sentiment.py
@@ -1,71 +1,81 @@
-fname = "yourfile.txt"
-with open(fname, 'r') as fileref:         # step 1
-    lines = fileref.readlines()           # step 2
-    for lin in lines:                     # step 3
-        #some code that references the variable lin
-#some other code not relying on fileref   # step 4
+input_file = open('assets/project_twitter_data.csv','r')
+output_file = open('assets/resulting_data.csv','w')
 
-with open(fname, 'r') as fileref:
- for lin in fileref:
-     ## some code that uses line as the current line of the file
-     ## some more code
+punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
 
-filename = "squared_numbers.txt"
-outfile = open(filename, "w")
+#strip tweets of punctuation
 
-for number in range(1, 13):
-    square = number * number
-    outfile.write(str(square) + "\n")
-
-outfile.close()
-
-infile = open(filename, "r")
-print(infile.read()[:10])
-infile.close()
+def strip_punctuation(tweet_text):
+    cl_tweet = tweet_text
+    for char in punctuation_chars:
+        cl_tweet = cl_tweet.replace(char,'')
+        
+    return cl_tweet
 
 
+# positive word count function
+
+positive_words = []
+with open("assets/positive_words.txt") as pos_f:
+    for lin in pos_f:
+        if lin[0] != ';' and lin[0] != '\n':
+            positive_words.append(lin.strip())
+
+def get_pos(tweet_text):
+    tweet_lwr = strip_punctuation(tweet_text.lower())
+    pos_count = 0
+    for word in tweet_lwr.split():
+        if word in positive_words:
+            pos_count += 1
+        else:
+            continue
+    return pos_count 
 
 
+#negative word count function
 
-fileconnection = open("olympics.txt", 'r')
-lines = fileconnection.readlines()
-header = lines[0]
-field_names = header.strip().split(',')
-print(field_names)
-for row in lines[1:]:
-    vals = row.strip().split(',')
-    if vals[5] != "NA":
-        print("{}: {}; {}".format(
-                vals[0],
-                vals[4],
-                vals[5]))
+negative_words = []
+with open("assets/negative_words.txt") as neg_f:
+    for lin in neg_f:
+        if lin[0] != ';' and lin[0] != '\n':
+            negative_words.append(lin.strip())
+            
+def get_neg(tweet_text):
+    cl_tweet_lwr = strip_punctuation(tweet_text.lower())
+    neg_count = 0
+    for word in cl_tweet_lwr.split():
+        if word in negative_words:
+            neg_count += 1
+        else:
+            continue
+    return neg_count
 
+# write headers to the output file
+output_file.write('Number of Retweets, Number of Replies, Positive Score, Negative Score, Net Score\n')
 
-n = [0] * 12
-for i in range(1,13):
-    n[i-1] = i *12
-outfile = open("Multiples of 12", "w")
-for j in range(0, len(n)):
-    outfile.write(str(j+1) + ',' + str(n[j]))
-    # +1 to j since the array starts at 0 and we start counting at 1
-    outfile.write('\n')
-outfile.close()
+# skip over the header before starting the input loop
+input_file.readline()
 
+# INPUT LOOP
+# step 1/4 : clean each twitter line of punctuation
 
+for line in input_file:
+    row = line.strip().split(',')
 
+# step 2/4: creation of variables: extract tweet_text, retweet_count, reply_count
+    tweet_text = row[0]
+    retweet_count = row[1]
+    reply_count = row[2]
+    
+# step 3/4: calculate positive, negative, net scores
+    pos_score = get_pos(tweet_text)
+    neg_score = get_neg(tweet_text)   
+    net_score = pos_score - neg_score
+    
+# step 4/4: write the results to the output file
+    row_string = '{},{},{},{},{}'.format(retweet_count, reply_count, pos_score, neg_score, net_score)
+    output_file.write(row_string)
+    output_file.write('\n')
 
-olympians = [("John Aalberg", 31, "Cross Country Skiing"),
-             ("Minna Maarit Aalto", 30, "Sailing"),
-             ("Win Valdemar Aaltonen", 54, "Art Competitions"),
-             ("Wakako Abe", 18, "Cycling")]
-
-outfile = open("reduced_olympics.csv", "w")
-# output the header row
-outfile.write('Name,Age,Sport')
-outfile.write('\n')
-# output each of the rows:
-for olympian in olympians:
-    row_string = '{},{},{}'.format(olympian[0], olympian[1], olympian[2])
-    outfile.write(row_string)
-    outfile.write('\n')
-outfile.close()
+input_file.close()
+output_file.close()
